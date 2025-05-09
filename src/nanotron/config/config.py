1c077a8762af47542e72527a6f7426789d316c8f
Omkar Pangarkar <omkarenator@gmail.com>
2025-05-09 19:51:01 -0400

    parse tokenizer correctly

--- src/nanotron/config/config.py
+++ src/nanotron/config/config.py
@@ -185,6 +185,9 @@ class NanosetDatasetsArgs:
                     first_line = f.readline().strip()
                     if "|" in first_line:
                         tokenizer_name, token_size_in_bytes = first_line.split("|")
+                        tokenizer_path = Path(tokenizer_name)
+                        if tokenizer_path.is_file():
+                            tokenizer_name = tokenizer_path.parent
                         if self.tokenizer_name is None:
                             self.tokenizer_name = tokenizer_name
                             self.token_size_in_bytes = int(token_size_in_bytes)
